17110b488bf872be77d535095d8d40d144700566
Eron Rodrigo Fagundes <eronrodrigof@yahoo.com.br>
2019-08-28 19:44:38 -0300

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import Header from './Header'
+import Generos from './Generos'
+import NovoGenero from './NovoGenero'
+import EditarGenero from './EditarGenero'
+import {
+  BrowserRouter as Router, //Roteador
+  Route                    //Rota
+} from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <h1>Home</h1>
+  )
+}
+
+function App() {
+  const [data, setData] = useState({})
+useEffect(() => {
+  axios.get('/api').then(res => {
+    setData(res.data)
+  })
+})
+
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Route path='/' exact component={Home}/>
+        <Route path='/generos/editar/:id' exact component={EditarGenero}/>
+        <Route path='/generos/novo' exact component={NovoGenero}/>
+        <Route path='/generos' exact component={Generos}/>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
